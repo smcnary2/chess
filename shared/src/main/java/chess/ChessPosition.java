bf2ba876fc570b7f11c480f18dac1fb9bf9b0d4b
sofia <sofiamcnary@gmail.com>
2024-01-18 11:30:16 -0700

    fixed project structure

---
+++ shared/src/main/java/chess/ChessPosition.java
@@ -0,0 +1,38 @@
+package chess;
+
+/**
+ * Represents a single square position on a chess board
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessPosition {
+    //only holds row and column
+    //an object for each chess piece
+    public int row;
+    public int column;
+    public ChessPosition(int row, int col) {
+        this.row = row;
+        column = col;
+    }
+
+    /**
+     * @return which row this position is in
+     * 1 codes for the bottom row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * @return which column this position is in
+     * 1 codes for the left row
+     */
+    public int getColumn() {
+        return column;
+    }
+
+    //toString
+    //equals
+    //hashCode
+}
